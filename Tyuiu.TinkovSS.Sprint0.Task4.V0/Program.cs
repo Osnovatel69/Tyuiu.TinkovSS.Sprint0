174a5b369537e8caa35e542b278ab1d0c2354367
TinkovSS <tinkovsergey05@mail.ru>
2026-09-11 14:38:29 +0500

    1

---
+++ Tyuiu.TinkovSS.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.TinkovSS.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
